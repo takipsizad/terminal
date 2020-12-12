8a015aabefad6187e82aeebf37757c5bd742364c
takipsizad <addeniz25@gmail.com>
2020-12-12 14:11:28 +0300

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -1,12 +1,23 @@
 import platform
+import subprocess
+import sys
 system = platform.platform()
 release = platform.release()
 version = platform.version()
 
+def systemcheck():
+    if 'system' == cmd:
+        system()
 def system():
     print (platform.machine())
     print (platform.platform())
 
-cmd = input()
-if 'system' == cmd:
-    system()
+
+
+
+def fullosname():
+     print (platform.uname())
+
+while True:
+     cmd = input()
+    
